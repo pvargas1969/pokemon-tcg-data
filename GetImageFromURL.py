5f532df5e632d00af7c6b290fd3fce44838f8c88
pvargas1969 <pato.vargas@gmail.com>
2022-11-02 00:04:20 -0400

    Elimina

--- GetImageFromURL.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url_imagen = "https://images.pokemontcg.io/swsh9/13_hires.png"      # El link de la imagen
-nombre_local_imagen = "swsh9-13.png"                                      # El nombre con el que queremos guardarla
-imagen = requests.get(url_imagen).content
-with open(nombre_local_imagen, 'wb') as handler:
-	handler.write(imagen)
